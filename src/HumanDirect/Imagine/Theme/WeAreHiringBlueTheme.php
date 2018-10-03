72fe9e8e97e5b867115e2b91aa76b2d0749493b0
Balazs Csaba <balazscsaba2006@yahoo.com>
2018-10-03 22:39:05 +0300

    Added WeAreHiring blue and indigo themes

---
+++ src/HumanDirect/Imagine/Theme/WeAreHiringBlueTheme.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HumanDirect\Imagine\Theme;
+
+/**
+ * Class WeAreHiringBlueTheme.
+ */
+class WeAreHiringBlueTheme extends WeAreHiringBlackTheme
+{
+    protected $boxBgColor = 'rgba(27, 179, 219, 0.7)';
+}
---
+++ src/HumanDirect/Imagine/Theme/WeAreHiringIndigoTheme.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HumanDirect\Imagine\Theme;
+
+/**
+ * Class WeAreHiringIndigoTheme.
+ */
+class WeAreHiringIndigoTheme extends WeAreHiringBlackTheme
+{
+    protected $boxBgColor = 'rgba(43, 57, 132, 0.7)';
+}
